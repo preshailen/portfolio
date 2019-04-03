8560e4ad6195fd31a99b41b4c0bfd1bf3242b2d4
Preshailen <ruthnampresh@gmail.com>
2019-04-03 23:48:17 +0200

    Edited server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '/dist/portfolio')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));

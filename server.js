1ef87e8fd7fa8c0f17f8c4e96b50fcb816b2bf1f
Preshailen <ruthnampresh@gmail.com>
2019-04-08 17:31:05 +0200

    Edited server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/dist/portfolio')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
 });
 

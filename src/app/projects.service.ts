3379a7e1e59777bacbd0023583c408760c14fa5f
Preshailen <ruthnampresh@gmail.com>
2019-04-09 14:15:01 +0200

    Edited Server

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
   uri = 'http://localhost:4000/projects';
+  baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
@@ -18,12 +20,12 @@ export class ProjectsService {
        description: pDescription
     };
     console.log(obj);
-    this.http.post(`${this.uri}/add`, obj)
+    this.http.post(`${this.baseUrl}/add`, obj)
         .subscribe(res => console.log('Done'));
   }
   getProjects() {
     return this
                .http
-               .get(`${this.uri}`);
+               .get(`${this.baseUrl}`);
   }
 }
